9633867d0f0267f2182b9c106a84f532dcef55ed
ll21202210 <llllllll@outlook.com>
2025-06-19 19:19:34 +0800

    first

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+int main(void)
+{
+
+
+    printf("const chaar");
+    return 0;
+}
+
+
+
+void conflict(void)
+{
+    // user1add
+    // public static void main(String args[])
+}
